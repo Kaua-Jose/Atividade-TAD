79ba7d38c429cdeb071052ae6fe5f05b4e6a1929
Kaua-Jose <kaua.jose11222@outlook.com.br>
2023-05-16 16:05:22 -0300

    Neymar

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace TAD
     {
         static void Main(string[] args)
         {
-            
+            JogadorFutebol jogador1 = new JogadorFutebol(
+                "Neymar Junior",
+                "NEYMAR JR",
+                "05/02/1992",
+                "Ponta Esquerda",
+                true,
+                "Direito",
+                293,
+                188,
+                "Puskas, Paulista, Copa do Brasil, Libertadores, Champions League, Mundial, Recopa Sulamericana, La Liga, Ligue 1",
+                68,
+                1.75,
+                135,
+                9,
+                "Paris Saint German",
+                true
+            );
+
+            jogador1.imprimir();
         }
     }
 }
--- Struct/JogadorFutebol.cs
+++ Struct/JogadorFutebol.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Struct;
 
 public struct JogadorFutebol
@@ -18,12 +20,56 @@ public struct JogadorFutebol
     public string titulos { get; set; }
     public bool jogouNoPeixao { get; set; }
 
-    public JogadorFutebol(string nomeCompleto, string nomeCamisa, string dataNascimento, string posicaoPreferida, bool ambidestro, string pePreferido, int numeroDeGols, int numeroDeAssistencias, string titulos, float peso, float altura, int cartoesAmarelos, int cartoesVermelhos, string clubeAtual, bool jogouNoPeixao)
+    public JogadorFutebol(string nomeCompleto, string nomeCamisa, string dataNascimento,
+    string posicaoPreferida, bool ambidestro, string pePreferido, int numeroDeGols, 
+    int numeroDeAssistencias, string titulos, float peso, float altura, int cartoesAmarelos,
+    int cartoesVermelhos, string clubeAtual, bool jogouNoPeixao)
     {
         this.nomeCompleto = nomeCompleto;
         this.nomeCamisa = nomeCamisa;
         this.dataNascimento = dataNascimento;
         this.posicaoPreferida = posicaoPreferida;
-        this.ambidestro = ambidestro
+        this.ambidestro = ambidestro;
+        this.pePreferido = pePreferido;
+        this.numeroDeGols = numeroDeGols;
+        this.numeroDeAssistencias = numeroDeAssistencias;
+        this.titulos = titulos;
+        this.peso = peso;
+        this.altura = altura;
+        this.cartoesAmarelos = cartoesAmarelos;
+        this.cartoesVermelhos = cartoesVermelhos;
+        this.clubeAtual = clubeAtual;
+        this.jogouNoPeixao = jogouNoPeixao;
+    }
+
+    public void registrarNumeroCartoesAmarelos(int numeroDeCartoes)
+    {
+        this.cartoesAmarelos = this.cartoesAmarelos + numeroDeCartoes;
+        Console.WriteLine("O número de cartões amarelos foi atualizado.");
+    }
+    public void registrarNumeroCartoesVermelhos(int numeroDeCartoes)
+    {
+        this.cartoesVermelhos = this.cartoesVermelhos + numeroDeCartoes;
+        Console.WriteLine("O número de cartões vermelhos foi atualizado.");
+    }
+    public void imprimir()
+    {
+        Console.WriteLine(
+            "O nome do jogador é: "+this.nomeCompleto+
+            "\nO nome na camisa é: "+this.nomeCamisa+
+            "\nEle nasceu em: "+this.dataNascimento+
+            "\nEle pesa: "+this.peso+
+            "\nEle mede: "+this.altura+
+            this.ambidestro ? "\nEle é ambidestro" : "\nEle não é ambidestro"+
+            " e seu pé preferido é: "+this.pePreferido+
+            string.IsNullOrEmpty(this.clubeAtual) ? "\nEle não tem vinculo com nenhum clube" : $"\nEle tem um vinculo com o clube: {this.clubeAtual}"+
+            "\nA posição preferida dele é: "+this.posicaoPreferida+
+            "\nEle fez "+this.numeroDeGols+" gols na carreira"+
+            "\nEle deu "+this.numeroDeAssistencias+" assistencias na carreira"+
+            string.IsNullOrEmpty(this.titulos) ? "\nEle não tem conquistou nenhum titulo na carreira" : $"\nEle conquistou os seguintes titulos na carreira: {this.titulos}"+
+            string.IsNullOrEmpty(this.cartoesAmarelos) ? "\nEle não tomou nenhum cartão amarelo na carreira" : $"\nEle tomou {this.cartoesAmarelos} cartoes amarelos na carreira"+
+            string.IsNullOrEmpty(this.cartoesAmarelos) ? "\nEle não tomou nenhum cartão vermelho na carreira" : $"\nEle tomou {this.cartoesVermelhos} cartoes vermelhos na carreira"+
+            this.jogouNoPeixao ? "\nE ele não jogou no Peixão" : "\nE ele jogou no Peixão"  
+        );
     }
 }
